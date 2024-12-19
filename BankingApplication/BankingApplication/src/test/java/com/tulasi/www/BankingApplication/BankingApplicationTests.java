7dfae460f4e6ba898bebda5d008aadaab4a57541
tulasi <tulasi@gmail.com>
2024-12-19 18:36:23 +0530

    first commit

---
+++ BankingApplication/BankingApplication/src/test/java/com/tulasi/www/BankingApplication/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tulasi.www.BankingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
